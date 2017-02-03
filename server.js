6df0e1af5828cc77411277cbbe36e58097548829
ffsommers <ssommers21@gmail.com>
2017-02-03 13:56:56 -0800

    configures port for hosting on heroku.

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var request = require('request').defaults({ encoding: null });
+var port = process.env.PORT || CONFIG.port;
+app.listen(port);
