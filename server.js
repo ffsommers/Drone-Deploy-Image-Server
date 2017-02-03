0dedb305dcd1c56ae142e1b2440a55125a172875
ffsommers <ssommers21@gmail.com>
2017-02-03 13:56:15 -0800

    requires request module in server.js

--- server.js
+++ server.js
@@ -2,3 +2,4 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var request = require('request').defaults({ encoding: null });
