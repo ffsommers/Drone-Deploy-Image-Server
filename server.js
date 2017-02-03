2ff44079f04ac6c6a5206b5eb62abee526d08d8b
ffsommers <ssommers21@gmail.com>
2017-02-03 13:58:48 -0800

    configures server.js for cors whitelisting dronedeploy.

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ var bodyParser = require('body-parser');
 var request = require('request').defaults({ encoding: null });
 var port = process.env.PORT || CONFIG.port;
 app.listen(port);
+
+var whitelist = ['https://www.dronedeploy.com'];
+var corsOptions = {
+  origin: function (origin, callback) {
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted)
+  }
+}
