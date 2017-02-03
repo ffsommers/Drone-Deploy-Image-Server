1b5ff4243a17d3834d4dff0b09db965538a591bc
ffsommers <ssommers21@gmail.com>
2017-02-03 13:59:29 -0800

    sets CORS config to be used for post route.

--- server.js
+++ server.js
@@ -13,3 +13,9 @@ var corsOptions = {
     callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted)
   }
 }
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
